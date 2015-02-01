2f1909615759d64ba81827b1128212db1c46b01c
koukogildas <gildaskouko@gmail.com>
2015-01-31 22:50:00 -0500

    Ma première version

---
+++ test_console_application.dart
@@ -0,0 +1,40 @@
+// Define a function.
+printNumber(num aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where the app starts executing.
+main() {
+  var number = 42; // Declare and initialize a variable.
+  printNumber(number); // Call a function.
+  
+  
+// String -> int
+var one = int.parse('1');
+assert(one == 1);
+
+// String -> double
+var onePointOne = double.parse('1.1');
+assert(onePointOne == 1.1);
+
+// int -> String
+String oneAsString = 1.toString();
+assert(oneAsString == '1');
+
+// double -> String
+String piAsString = 3.14159.toStringAsFixed(2);
+assert(piAsString == '3.14');
+
+var s = r"In a raw string, even \n isn't special.";
+print(s);
+// Unicode escapes work: [heart]
+print('Unicode escapes work: \u2668');
+
+if (1) {
+  print('JS prints this line.');
+} else {
+  print('Dart in production mode prints this line.');
+  // However, in checked mode, if (1) throws an
+  // exception because 1 is not boolean.
+}
+}
